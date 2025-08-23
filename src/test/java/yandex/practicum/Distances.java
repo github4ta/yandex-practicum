dd044cc102da4f7fbf3c4da9d61972f32c53a6d5
Sergei Tsarik <serge.tsarik@gmail.com>
2025-08-23 22:29:08 +0300

    add utils

---
+++ src/test/java/yandex/practicum/Distances.java
@@ -0,0 +1,8 @@
+package yandex.practicum;
+
+public class Distances {
+
+    public static int generateDistanceInRange(int begin, int end) {
+        return (int) (begin + (end - begin + 1) * Math.random());
+    }
+}
