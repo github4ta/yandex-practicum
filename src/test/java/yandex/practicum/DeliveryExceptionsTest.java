791b6d049a5ff12bdc5409f9c98cc4eea7add6cc
Sergei Tsarik <serge.tsarik@gmail.com>
2025-08-23 22:29:26 +0300

    add tests

--- src/test/java/yandex/practicum/DeliveryExceptionsTest.java
+++ src/test/java/yandex/practicum/DeliveryExceptionsTest.java
@@ -13,7 +13,7 @@ public class DeliveryExceptionsTest {
     @Test
     @DisplayName("Test CargoDeliveryException")
     public void test1() {
-        int distance = -1;
+        int distance = Distances.generateDistanceInRange(Integer.MIN_VALUE, -1);
         CargoDeliveryException thrown = Assertions.assertThrows(CargoDeliveryException.class, () -> {
             Delivery.calculateShippingCost(distance, Dimensions.LARGE, false, ServiceLoad.INCREASED);
         });
@@ -22,12 +22,47 @@ public class DeliveryExceptionsTest {
     }
 
     @Test
-    @DisplayName("Test FragileCargoDeliveryException")
+    @DisplayName("Test CargoDeliveryException")
     public void test2() {
+        int distance = Distances.generateDistanceInRange(Integer.MIN_VALUE, -1);
+        CargoDeliveryException thrown = Assertions.assertThrows(CargoDeliveryException.class, () -> {
+            Delivery.calculateShippingCost(distance, Dimensions.LARGE, true, ServiceLoad.INCREASED);
+        });
+
+        Assertions.assertEquals(String.format("Distance cannot be negative. Your distance is %s.", distance), thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test FragileCargoDeliveryException")
+    public void test20() {
         final int MAX_DISTANCE_30_KM = 30;
-        int distance = 31;
+        int distance = Distances.generateDistanceInRange(31, Integer.MAX_VALUE);
         FragileCargoDeliveryException thrown = Assertions.assertThrows(FragileCargoDeliveryException.class, () -> {
             Delivery.calculateShippingCost(distance, Dimensions.LARGE, true, ServiceLoad.INCREASED);
+        });
+
+        Assertions.assertEquals(String.format("Fragile cargo cannot be transported over a distance of more than %s km. Your distance is %s.", MAX_DISTANCE_30_KM, distance), thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test FragileCargoDeliveryException")
+    public void test21() {
+        final int MAX_DISTANCE_30_KM = 30;
+        int distance = Distances.generateDistanceInRange(31, Integer.MAX_VALUE);
+        FragileCargoDeliveryException thrown = Assertions.assertThrows(FragileCargoDeliveryException.class, () -> {
+            Delivery.calculateShippingCost(distance, Dimensions.SMALL, true, ServiceLoad.INCREASED);
+        });
+
+        Assertions.assertEquals(String.format("Fragile cargo cannot be transported over a distance of more than %s km. Your distance is %s.", MAX_DISTANCE_30_KM, distance), thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test FragileCargoDeliveryException")
+    public void test22() {
+        final int MAX_DISTANCE_30_KM = 30;
+        int distance = Distances.generateDistanceInRange(31, Integer.MAX_VALUE);
+        FragileCargoDeliveryException thrown = Assertions.assertThrows(FragileCargoDeliveryException.class, () -> {
+            Delivery.calculateShippingCost(distance, Dimensions.LARGE, true, ServiceLoad.OTHER);
         });
 
         Assertions.assertEquals(String.format("Fragile cargo cannot be transported over a distance of more than %s km. Your distance is %s.", MAX_DISTANCE_30_KM, distance), thrown.getMessage());
